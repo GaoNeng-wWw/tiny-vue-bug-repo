c28f0b1e3edfbaa825acff90d66982e8ef857289
GaoNeng-wWw <gaonenggg@outlook.com>
2024-09-21 20:45:16 +0800

    feat: add build workflow

--- farm.config.ts
+++ farm.config.ts
@@ -2,10 +2,34 @@ import { defineConfig } from '@farmfe/core';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  vitePlugins: [vue()],
+  vitePlugins: [
+    vue(),
+  ],
   compilation: {
     define: {
       'process.env': {...process.env}
     }
-  }
+  },
+  plugins: [
+    {
+      name: 'unmini',
+      priority: 0,
+      transform: {
+        filters: {
+          resolvedPaths: ['']
+        },
+        executor(params) {
+          if(params.moduleId.endsWith('css')) {
+            const reg = /(.)\}/gm;
+            return {
+              content: params.content.replace(reg, '$1;'),
+            }
+          }
+          return {
+            ...params
+          }
+        },
+      }
+    }
+  ]
 });
